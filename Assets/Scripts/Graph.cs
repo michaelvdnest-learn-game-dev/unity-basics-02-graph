d1af24c275ef859ed4d505148bfc88705c985c9b
Michael van der Nest <michaelvdnest@gmail.com>
2023-12-23 08:14:39 +0200

    Create a simple Surface Shader

--- Assets/Scripts/Graph.cs
+++ Assets/Scripts/Graph.cs
@@ -6,25 +6,28 @@ public class Graph : MonoBehaviour
 {
 
     [SerializeField]
-	Transform pointPrefab;
+    Transform pointPrefab;
 
     [SerializeField, Range(10, 100)]
-	int resolution = 10;
+    int resolution = 10;
 
     // Start is called before the first frame update
     void Start()
     {
         float step = 2f / resolution;
-		Vector3 position = Vector3.zero;
+        Vector3 position = Vector3.zero;
         Vector3 scale = Vector3.one * step; 
-		for (int i = 0; i < resolution; i++) {
-			Transform point = Instantiate(pointPrefab);
-			position.x = (i + 0.5f) * step - 1f;
-            position.y = position.x * position.x;
-			point.localPosition = position;
-			point.localScale = scale;
+        for (int i = 0; i < resolution; i++) {
+            Transform point = Instantiate(pointPrefab);
+            position.x = (i + 0.5f) * step - 1f;
+
+            // Graph formula
+            position.y = position.x * position.x * position.x;
+
+            point.localPosition = position;
+            point.localScale = scale;
             point.SetParent(transform, false);
-		}
+        }
     }
 
     // Update is called once per frame
